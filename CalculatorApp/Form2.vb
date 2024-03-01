096c63dca269eb72e38a3fe9060b4c415deb2412
123hisul <kibuiluis@gmail.com>
2024-03-01 09:47:49 +0300

    admin login

--- CalculatorApp/Form2.vb
+++ CalculatorApp/Form2.vb
@@ -2,4 +2,21 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub btn_admin_Click(sender As Object, e As EventArgs) Handles btn_admin.Click
+        Dim username, password As String
+        username = txtusername.Text
+        password = txtpassword.Text
+
+        If username = "Admin" And password = "Pass" Then
+            Form1.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Wrong Username or Password!")
+            Me.Show()
+            txtusername.Clear()
+            txtpassword.Clear()
+            txtusername.Focus()
+        End If
+    End Sub
 End Class
